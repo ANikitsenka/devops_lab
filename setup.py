ee945b267cbe577319f3534c28f3f4748b51df9d
ANikitsenka <52278468+ANikitsenka@users.noreply.github.com>
2019-10-28 10:00:27 +0300

    Update setup.py
    String 6:
        change 'scripts=[],' to 'scripts=["snapshot.py"],'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="snapshot",
     packages=find_packages(),
-    scripts=[],
+    scripts=["snapshot.py"],
     version="0.1",
     author="Andrei Nikitsenka",
     author_email="andrei_nikitsenka@epam.com",
