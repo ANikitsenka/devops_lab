6157764bce9d6f031b9fe541586180a0498ec820
ANikitsenka <52278468+ANikitsenka@users.noreply.github.com>
2019-10-30 13:13:41 +0300

    Update task2.py
    Remove parameters' definitions, that has appropriate defaults values.

--- task2.py
+++ task2.py
@@ -3,7 +3,7 @@ b = input()
 
 if int(a) > 0:
     a = sorted(a, reverse=True)
-    for i in range(0, len(a)):
+    for i in range(len(a)):
         a[i] = int(a[i])
     a_out = sum(d * 10 ** i for i, d in enumerate(a[::-1]))
 else:
@@ -11,9 +11,9 @@ else:
     a = sorted(a)
     if a[0] == '0':
         a.insert(2, '0')
-    for i in range(0, len(a)):
+    for i in range(len(a)):
         a[i] = int(a[i])
-    a_out = 0 - sum(d * 10 ** i for i, d in enumerate(a[::-1]))
+    a_out = - sum(d * 10 ** i for i, d in enumerate(a[::-1]))
 
 if int(b) > 0:
     b = sorted(b)
@@ -27,6 +27,6 @@ else:
     b = sorted(b, reverse=True)
     for i in range(0, len(b)):
         b[i] = int(b[i])
-    b_out = 0 - sum(d * 10 ** i for i, d in enumerate(b[::-1]))
+    b_out = - sum(d * 10 ** i for i, d in enumerate(b[::-1]))
 
 print(a_out - b_out)
